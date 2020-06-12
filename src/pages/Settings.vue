a263b4a13eb5a697060cbf85620563d2d6a536a0
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2020-06-12 14:43:09 +0200

    feat(settings): change name

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -14,10 +14,19 @@
             q-item
               q-item-section(avatar)
                 q-icon(name="person")
-              q-item-section
+              q-item-section(v-if="editingName")
+                q-input(v-model="inputName"
+                  :rules="[val => !!val || 'Required']"
+                  @keydown.enter.prevent="changeName"
+                  @blur="editingName = false"
+                  label="Name"
+                  borderless dense autofocus hide-bottom-space)
+                  template(v-slot:append)
+                    q-btn(round dense flat icon="save_alt" @click="changeName")
+              q-item-section(v-else)
                 q-item-label(caption) Name
                 q-item-label {{name}}
-              q-item-section(side top)
+              q-item-section(v-if="!editingName" side top)
                 q-btn(icon="create" flat round color="primary" @click="editName")
             q-separator
 </template>
@@ -29,17 +38,22 @@ import { store } from 'src/store'
 export default defineComponent({
   name: 'PageChat',
   setup() {
+    // TODO create an inline input component that wraps the 'set name' logic
     const name = computed<string>(() => store.getters['local/name'])
     const avatar = computed<string>(() => store.getters['local/avatar'])
     const inputName = ref('')
+    const editingName = ref(false)
     const editName = () => {
+      editingName.value = true
       inputName.value = name.value
     }
     const changeName = () => {
+      console.log('change name', inputName.value)
       store.dispatch('local/name', inputName.value)
+      editingName.value = false
     }
 
-    return { name, avatar, editName, changeName }
+    return { name, avatar, editName, editingName, changeName, inputName }
   }
 })
 </script>
