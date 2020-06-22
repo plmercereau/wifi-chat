b5b2175c3f2db9bfbf3ce994340c8e1d5f2a051c
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2020-06-22 14:46:11 +0200

    style: remove useless comments

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,23 +3,13 @@ import { store as storeWrapper } from 'quasar/wrappers'
 import Vuex, { Store } from 'vuex'
 import { GlobalState } from 'src/chat/store'
 
-// import example from './module-example';
-// import { ExampleStateInterface } from './module-example/state';
-
-/*
- * If not building with SSR mode, you can
- * directly export the Store instantiation
- */
-
 let store: Store<GlobalState>
 
 export default storeWrapper(function({ Vue }) {
   Vue.use(Vuex)
 
   store = new Vuex.Store<GlobalState>({
-    modules: {
-      // example
-    },
+    modules: {},
 
     // enable strict mode (adds overhead!)
     // for dev mode only
